f1d9fed4b6d9b4d220357ef4c6e83195f867decd
Daniel Urbanowicz <urbanowicz.daniel@gmail.com>
2020-07-27 08:41:42 +0200

    Initial commit

---
+++ routes/reviewRoutes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const reviewController = require('../controllers/rewiewController');
+const authController = require('../controllers/authController');
+
+const router = express.Router({ mergeParams: true });
+
+// POST /tours/234afjlf4/reviews
+// GET /tours/234afjlf4/reviews
+// POST /reviews
+
+router.use(authController.protect);
+
+router
+  .route('/')
+  .get(reviewController.getAllReviews)
+  .post(
+    authController.restrictTo('user'),
+    reviewController.setTourUserIds,
+    reviewController.createReview
+  );
+
+router
+  .route('/:id')
+  .get(reviewController.getReview)
+  .patch(authController.restrictTo('user', 'admin'), reviewController.updateReview)
+  .delete(authController.restrictTo('user', 'admin'), reviewController.deleteReview);
+
+module.exports = router;
